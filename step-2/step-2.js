9fb8c3b86bb52a69b4458597612380ff02d1dce2
waden10 <fantm10@hotmail.com>
2017-03-14 01:48:22 +0000

    step 2

---
+++ step-2/step-2.js
@@ -0,0 +1,34 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+var requestOptions = {
+  host: 'example.com',
+  path: '/'
+};
+
+var buf = "";
+
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    buf += data;
+
+  });
+
+
+  response.on('end', function() {
+    console.log('Response stream complete.', buf);
+  });
+
+});
+}
+
+getAndPrintHTML();
