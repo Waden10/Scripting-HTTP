53203c76c445a269984291633a3867273c49fbe7
waden10 <fantm10@hotmail.com>
2017-03-14 02:37:32 +0000

    step 3

---
+++ step-3/step-3.js
@@ -0,0 +1,32 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+  var buf = "";
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      buf += data;
+      // console.log('data received', data)
+
+    });
+
+
+    response.on('end', function() {
+      console.log('Response stream complete.', buf);
+    });
+
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+
+getAndPrintHTML(requestOptions);
