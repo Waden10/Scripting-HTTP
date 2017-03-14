4af41498f75dd53c8345149acd5a4b8ea24ac1d6
waden10 <fantm10@hotmail.com>
2017-03-14 00:47:54 +0000

    step 1

---
+++ step-1/step-1.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+
+
+  https.get(requestOptions, (res) => {
+    // console.log('statusCode:', res.statusCode);
+    // console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+      // process.stdout.write(d);
+      console.log("line start", d.toString('utf8'))
+    });
+
+  }).on('error', (e) => {
+    console.error(e);
+  });
+}
+
+getAndPrintHTMLChunks();
